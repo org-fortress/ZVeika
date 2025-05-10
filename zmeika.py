d246555598e413aa6b8bd9e50867f6f3243c7e91
rever <129592747+org-fortress@users.noreply.github.com>
2025-05-10 18:49:59 +0500

    zmeika.py

--- zmeika.py
+++ zmeika.py
@@ -2,8 +2,8 @@ from pygame import *
 from random import randint
 window = display.set_mode((700, 500))
 display.set_caption('Zмейка')
-background = transform.scale(image.load("фон.png"), (700,500))
-score = 0
+background = transform.scale(image.load("background.png"), (700,500))
+score = 1
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_speed, player_x, player_y, player_width, player_height):
         super().__init__()
@@ -22,9 +22,20 @@ class Player(GameSprite):
     def update(self):
         self.rect.y += self.speed_y
         self.rect.x += self.speed_x
+class Tail(GameSprite):
+    def __init__(self,player_image, player_x, player_y,size_x, size_y,player_speed, timer):
+        super().__init__(player_image, player_x, player_y,size_x, size_y, player_speed)
+        self.timer = timer
+    def update(self):
+        self.timer -= 1
+        print(self.timer)
+        if self.timer <= 0:
+            self.image = transform.scale(image.load("тело.png"), (50,50))
+            self.kill()
 food_spawn = randint(0,700)
 head = Player('голова.png',50,350,250,50,50)
 food = GameSprite('еда.png',0,randint(0,14)*50,randint(0,10)*50,50,50)
+tails = sprite.Group()
 wait = 0 
 clock = time.Clock()
 FPS = 60
@@ -33,7 +44,8 @@ finish = False
 while game:
     if wait == 0:
         if finish != True:
-            window.blit(background, (0,0))
+            # window.blit(background, (0,0))
+
             food.update()
             food.reset()
             head.update()
@@ -43,6 +55,13 @@ while game:
                 food.rect.x = randint(0,14)*50-50
                 food.rect.y = randint(0,10)*50-50
             wait = 30
+            t = Tail('тело.png', head.rect.x, head.rect.y, 50,50,0,score)
+            print(head.rect.x, head.rect.y)
+            print(t.rect.x, t.rect.y)
+            tails.add(t)
+            tails.update()
+            tails.draw(window)
+            print(tails)
     keys_pressed = key.get_pressed()
     if keys_pressed[K_w] and head.rect.y > 0:
         head.speed_y = -head.player_speed
